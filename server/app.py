fd390f32152f8ac4185f18d2caa25390333af01b
Evan Rupert <evan@studioreach.io>
2020-02-01 10:06:59 -0500

    somethingh

--- server/app.py
+++ server/app.py
@@ -1,9 +1,21 @@
 from flask import Flask, escape, request
+from flask_socketio import SocketIO
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
 
 
 @app.route('/')
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
+
+@socketio.on('message')
+def handle_message(message):
+  send('hello')
+  print('recieved message: ' + message)
+
+if __name__ == '__main__':
+  socketio.run(app)
